caf8fc40a5941fb38d02ed945914b860eb5d4dff
Kevin Dos Santos Alves <kevdos-s@student.42lausanne.ch>
2025-07-13 03:23:00 +0200

    segmentation fault after showing content file but iterating in this direction

--- mainKevin.c
+++ mainKevin.c
@@ -6,54 +6,43 @@
 /*   By: kevdos-s <kevdos-s@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 18:50:04 by kevdos-s          #+#    #+#             */
-/*   Updated: 2025/07/13 03:12:16 by kevdos-s         ###   ########.fr       */
+/*   Updated: 2025/07/13 03:18:51 by kevdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "global.h"
 
-int ft_size_file(int file_descriptor);
+char *ft_size_file(int file_descriptor);
 char *ft_create_memory_str(int nb_octet);
 
 int main()
 {
 	char *file_path = "./ressource/numbers.dict";
-	char *content_file_test;
 
 	int file_test = open(file_path, O_RDONLY);
 
-	//does't work ???
-	//exactly same code in the ft_size_file function
-	int max_octet = 400000000; //400 megaoctet
-	char *content_file;
-
-	int test = read(file_test, content_file, max_octet);
-	//end of copy from ft_size_file function
-
-	//BUT HERE IT WORK??
-	int octet_to_read = ft_size_file(file_test);
+	char *content = ft_size_file(file_test);
 
 	close(file_test);
-	//content_file_test = (char *)malloc(octet_to_read * sizeof(char));
 
-	int file_open = open(file_path, O_RDONLY);
 
-	read(file_open, content_file_test, octet_to_read);
-
-	close(file_open);
-
-	printf("%s\n", content_file_test);
+	printf("%s\n", content);
 
 }
 
-int ft_size_file(int file_descriptor)
+char *ft_size_file(int file_descriptor)
 {
-	int max_octet = 400000000; //400 megaoctet
+	int max_octet = 100000000; //100 megaoctet
 	char *content_file;
 
 	int test = read(file_descriptor, content_file, max_octet);
+	content_file[test] = '\0';
 
-	return (test);
+	//content_file = malloc(sizeof(test));
+
+	//read(file_descriptor, content_file, test);
+
+	return (content_file);
 }
 
 char *ft_create_memory_str(int nb_octet)
